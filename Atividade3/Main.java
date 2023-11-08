98b334416f2c820da8fb6c2a2f8ae37ec274d2b8
Silvio Fabian <silviofabian334@gmail.com>
2023-11-08 20:19:17 -0300

    fazendo atividade 3

---
+++ Atividade3/Main.java
@@ -0,0 +1,11 @@
+package Atividade3;
+
+public class Main {
+   public static void main(String[] args) {
+       int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+       int total = Summation.parallelSum(arr);
+       System.out.println("Soma total: " + total);
+   }
+}
+
